460220656debadf1d1942127b8de75757025eeb6
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2023-07-06 18:12:41 -0300

    Update dashboard demo

--- src/pages/dashboard_demo.tsx
+++ src/pages/dashboard_demo.tsx
@@ -2,6 +2,19 @@ import { Typography } from "@material-tailwind/react"
 import HeaderStat from "@/components/header_stat"
 import VerticalBarChart from "@/components/vertical_bar_chart"
 
+function LoremText() {
+  return(
+    <>
+      <Typography variant="h4" color="blue-gray">
+        Lorem Ipsum
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed eleifend nibh, ut semper nulla. Proin tristique ornare turpis auctor iaculis. Vestibulum consequat arcu at ex tempus interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam luctus ut augue vitae posuere. Nunc in nulla sed nisl volutpat fermentum. Mauris ante libero, aliquam et vestibulum et, rutrum at mi. Curabitur eget pellentesque tellus. Vestibulum placerat lacinia fermentum. Vivamus congue mauris ac libero dictum ullamcorper. Aliquam justo tortor, condimentum vel lobortis in, condimentum sed lectus. Morbi ac hendrerit libero, eget ornare sapien. Praesent interdum sed libero sed placerat. Cras velit eros, sodales sit amet imperdiet vel, dictum sit amet metus. Pellentesque sit amet libero at tortor sodales porta at auctor nunc.
+      </Typography>
+    </>
+  )
+}
+
 export default function DashboardDemo() {
   return(
     <>
@@ -30,9 +43,17 @@ export default function DashboardDemo() {
 
         </div>
         <hr className="my-2 border-blue-gray-50" />
-        <div className="max-w-[100%] flex justify-between flex-wrap">
+        <section className="max-w-[100%] my-10 flex justify-between flex-wrap">
           <VerticalBarChart className="w-1/2"/>
-        </div>
+          <div className="w-1/2">
+            <LoremText />
+          </div>
+        </section>
+        <section className="max-w-[100%] my-10 flex justify-between flex-wrap">
+          <div className="w-1/2">
+            <LoremText />
+          </div>
+        </section>
       </div>
     </>
   )
