8cbc17a5103f864fec2b5f3da265079ae1324e3d
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2023-07-06 18:11:51 -0300

    Add footer

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,12 +1,16 @@
 import type { PropsWithChildren } from 'react'
 import Sidebar from '@/components/sidebar'
+import Footer from '@/components/footer'
  
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <div className="flex">
+      <div className="flex h-full">
         <Sidebar />
-        <main className="w-4/5 w-full">{children}</main>
+        <main className="w-4/5 w-full">
+          {children}
+          <Footer></Footer>
+        </main>
       </div>
     </>
   )
